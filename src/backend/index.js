4bfe593ea2bcea9e2c226b6747d6b54b18902c60
Syed Huda <syed.huda@gmail.com>
2019-11-03 16:20:45 +0000

    Returns database query to response

--- src/backend/index.js
+++ src/backend/index.js
@@ -1,13 +1,21 @@
 'use string';
 
 var Percolator = require('percolator').Percolator;
+var dbSession = require('../../src/backend/dbSession.js');
 
 var port = 8080;
-var server = Percolator({'port': port});
+var server = Percolator({'port': port, 'autoLink': false});
 
 server.route('/api/keywords', {
-    GET: function (res, res) {
-        res.object({'foo': 'bar'}).send()
+    GET: function (req, res) {
+        dbSession.fetchAll('SELECT id, value, categoryID FROM keyword ORDER BY id', function(err, rows) {
+            if (err) {
+                console.log(err);
+                res.status.internalServerError(err);
+            } else {
+                res.collection(rows).send();
+            }
+        });
     }
 });
 
