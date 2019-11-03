efbaf3755d5456d0c5af4eb97bdd817a84b77f35
Syed Huda <syed.huda@gmail.com>
2019-11-03 14:46:25 +0000

    Adds expected results to test.

--- spec/e2e/apiSpec.js
+++ spec/e2e/apiSpec.js
@@ -3,6 +3,14 @@ var request = require('request');
 
 describe('The API', function() {
     it('should respond to a GET request at /api/keywords', function(done) {
+        var expected = {
+            "_items": [
+                {'id': 1, 'value': 'Aubergine', 'categoryID': 1},
+                {'id': 2, 'value': 'Onion', 'categoryID': 1},
+                {'id': 3, 'value': 'Knife', 'categoryID': 2},
+            ]
+        };
+
         request.get(
         {
             'url': 'http://localhost:8080/api/keywords/',
@@ -10,7 +18,7 @@ describe('The API', function() {
         },
         function (err, res, body) {
             expect(res.statusCode).toBe(200);
-            expect(body.foo).toEqual('bar');
+            expect(body).toEqual(expected);
             done();
         });
     });
